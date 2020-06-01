54bcc215f0ce0049cbb64e6332c7299b1a7c81ff
fritzgt <tuanis@live.com>
2020-06-01 11:28:23 +0300

    practice memory release

--- MemoryManagement/ViewController.m
+++ MemoryManagement/ViewController.m
@@ -25,6 +25,33 @@
     
     NSLog(@"Hi");
     
+    //Create object to test
+    NSString *name = [[NSString alloc] initWithString:@"Fritz"];
+    
+    NSLog(@"%@",name);
+    
+    //Makes the count go up by one
+    [name retain];
+    [name retain];
+//    NSLog(@"Counter before release: %@",name.retainCount);
+    //Release from memory
+    [name release];
+    [name release];
+    
+    [name release];
+    name = nil; //By setting it to nil we lower the memory position to zero
+    
+    //This could potencially crash the app (it may still work till something else use that memory space)
+    //NSLog(@"Counter after release: %@",name.retainCount);
+    
+    //All collection types will take ownership of memory
+    NSMutableArray *colors = [[NSMutableArray alloc] init];
+    
+    NSLog(@"%@",colors);
+    
+    
+    //clean up
+    [colors release];
 }
 
 
